d0047b107c59e13dd126e8c5e1389dc3c864ab8e
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-24 21:15:06 +0700

    Fix admin role check

---
+++ app/Http/Middleware/admin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use DB;
+use Confide;
+use Closure;
+use Redirect;
+
+class admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $a = Confide::user();
+        if ($a === null) {
+            return Redirect::to(route('user.login'));
+        }
+        $a = $a->toArray();
+        $q = DB::table('users_roles')->join('roles', 'users_roles.role_id', '=', 'roles.id', 'inner')->where('users_roles.user_id', $a['id'])->first();
+        if (isset($q->name) && $q->name === 'Admin') {
+            return $next($request);
+        } else {
+            ?>
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <script type="text/javascript">
+                    alert('You are not allowed to access this page!');
+                    window.location = '/';
+                </script>
+                <title>403 Forbidden</title>
+            </head>
+            </html>
+            <?php
+        }
+    }
+}
